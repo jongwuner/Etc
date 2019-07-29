bb1aa36d6a8fc292f6c7ef0a56456726e439a051
jongwuner <jklh0202@naver.com>
2019-07-30 00:21:46 +0900

    Hardcoding DLL Injection && DLL

---
+++ Hooking/DLL Injection_Notepad/DllCode/jjongchuDll1/dllmain.cpp
@@ -0,0 +1,21 @@
+﻿// dllmain.cpp : DLL 응용 프로그램의 진입점을 정의합니다.
+#include "stdafx.h"
+
+BOOL APIENTRY DllMain( HMODULE hModule,
+                       DWORD  ul_reason_for_call,
+                       LPVOID lpReserved
+                     )
+{
+    switch (ul_reason_for_call)
+    {
+	case DLL_PROCESS_ATTACH: {
+		MessageBox(nullptr, L"조선에 온 것을 환영하오.\n 한장 찍어보겠소?", L"활자인쇄소", MB_OKCANCEL);
+	}
+    case DLL_THREAD_ATTACH:
+    case DLL_THREAD_DETACH:
+    case DLL_PROCESS_DETACH:
+        break;
+    }
+    return TRUE;
+}
+
